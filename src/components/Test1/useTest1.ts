13ca9e2d3b8c2ed25ec0ab329f5c6bd109c05d59
javierquest <115017938+javierquest@users.noreply.github.com>
2022-10-19 18:53:41 -0300

    Imported from Quest

--- src/components/Test1/useTest1.ts
+++ src/components/Test1/useTest1.ts
@@ -9,7 +9,7 @@ const useTest1 = () => {
   const handle = (): any => {};
 
   const onClick = (param1:any,  param2: any,  param3: any,  param4: any): any => {};
-  const click = (param1: string, {...algo}: {algo: string}): any => {};
+  const click = (param1: string, {...algo}: {algo: string},  param3: any,  param4: any): any => {};
   let fns: any = {handle, click, click};
   return {data, fns};
 }
--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,7 @@
 
 
 
+
 export interface Test1Props {
   className?: any;
 };
